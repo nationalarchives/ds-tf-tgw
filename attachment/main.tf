f524dbd783ce41bd1211f9777e9876022364c536
milh0use <steven.hirchorn@gmail.com>
2020-11-17 15:55:22 +0000

    Fix syntax

--- attachment/main.tf
+++ attachment/main.tf
@@ -27,10 +27,16 @@ resource "aws_ec2_transit_gateway_vpc_attachment" "transit_gateway_attachment" {
 
 resource "aws_ec2_transit_gateway_vpc_attachment_accepter" "accept_tgw_attachment_in_intersite" {
   provider                      = "aws.intersite"
-  transit_gateway_attachment_id = "${aws_ec2_transit_gateway_vpc_attachment.transit_gateway_attachment.id}"
+  transit_gateway_attachment_id = aws_ec2_transit_gateway_vpc_attachment.transit_gateway_attachment.id
 
   tags = {
-    Terraform = "True"
+    Name        = var.name
+    Terraform   = "true"
+    Environment = var.environment
+    Account     = var.account
+    CreatedBy   = var.created_by
+    Owner       = var.owner
+    CostCentre  = var.cost_centre
   }
 }
 
